040b674a0a9b8cc8c0a4fc509fa832ea9c66a015
shoeib_shargo <shoeib.shargo@vivacomsolutions.com>
2022-11-05 17:36:01 +0600

    added locators in WomenPage class

--- src/test/java/UI_Automation/Pages/WomenPage.java
+++ src/test/java/UI_Automation/Pages/WomenPage.java
@@ -1,10 +1,28 @@
 package UI_Automation.Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class WomenPage {
     WebDriver driver;
+
+    @FindBy(className = "navigation_page")
+    WebElement navWomen;
+    @FindBy(xpath = "//label/a[contains(text(), 'Dresses')]")
+    WebElement chkDresses;
+    @FindBy(xpath = "//label/a[contains(text(), 'Dresses')]/span")
+    WebElement txaDressesCount;
+    @FindBy(xpath = "//label/a[contains(text(), 'Yellow')]")
+    WebElement chkYellow;
+    @FindBy(xpath = "//label/a[contains(text(), 'Yellow')]/span")
+    WebElement txaYellowCount;
+    @FindBy(xpath = "//span[@class='heading-counter']/span")
+    WebElement txaTotalProductCount;
+    @FindBy(xpath = "//ul[@class='product_list grid row']/li")
+    WebElement lstProductRow;
+
 
     public WomenPage(WebDriver driver){
         this.driver = driver;
